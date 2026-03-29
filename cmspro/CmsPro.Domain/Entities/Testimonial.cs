595dec1b6ce32a2ca1e0c75421d5dbd9a8c4b975
YoshuaPariona <loganpariona.1207@gmail.com>
2026-03-28 21:15:07 -0500

    feat: Añadir las entidades y enums del esquema

--- cmspro/CmsPro.Domain/Entities/Testimonial.cs
+++ cmspro/CmsPro.Domain/Entities/Testimonial.cs
@@ -5,39 +5,53 @@ public class Testimonial
 {
     public Guid Id { get; private set; }
 
-    public string Content { get; private set; } = string.Empty;
+    public string AuthorName { get; private set; }
+    
+    public string Content { get; private set; }
 
-    public string? ImageUrl { get; private set; }
-
-    public string? VideoUrl { get; private set; }
-
+    public string? MultimediaUrl { get; private set; }
+    
     public TestimonialStatus Status { get; private set; }
+    
+    public MultimediaType Format { get; private set; }
 
     public DateTime CreatedAt { get; private set; }
 
     public DateTime? ApprovedAt { get; private set; }
-    public DateTime? DummyField { get; private set; }
-
+    
+    public bool IsDeleted { get; private set; }
+    
+    // Relación con Categoría
+    public Guid CategoryId { get; private set; }
+    
+    public Category? Category { get; private set; }
+    
     // Constructor principal
-    public Testimonial(string content, string? imageUrl, string? videoUrl)
+    public Testimonial(string authorName, string content, Guid categoryId, MultimediaType format, string? multimediaUrl)
     {
+        
+        if (string.IsNullOrWhiteSpace(authorName)) throw new ArgumentException("El campo Author es requerido.");
+        if (string.IsNullOrWhiteSpace(authorName)) throw new ArgumentException("El campo Content es requerido.");
+
         Id = Guid.NewGuid();
+        AuthorName = authorName;
         Content = content;
-        ImageUrl = imageUrl;
-        VideoUrl = videoUrl;
-        Status = TestimonialStatus.Pending;
+        CategoryId = categoryId;
+        Format = format;
+        MultimediaUrl = multimediaUrl;
+        
+        Status = TestimonialStatus.Draft;
         CreatedAt = DateTime.UtcNow;
-        DummyField = DateTime.UtcNow;
     }
+    
 
-    // Métodos de dominio (IMPORTANTE 🔥)
-
-    public void Approve()
+    // Métodos de dominio
+    public void Publish()
     {
         if (Status != TestimonialStatus.Pending)
-            throw new InvalidOperationException("Only pending testimonials can be approved.");
+            throw new InvalidOperationException("Only pending testimonials can be published.");
 
-        Status = TestimonialStatus.Approved;
+        Status = TestimonialStatus.Published;
         ApprovedAt = DateTime.UtcNow;
     }
 
