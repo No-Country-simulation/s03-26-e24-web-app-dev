40c08ed6890c43032aafb148f4a76cbc9c26c1a3
YoshuaPariona <loganpariona.1207@gmail.com>
2026-03-28 22:01:44 -0500

    feat: Aplicar migraciones al esquema en supabase

--- cmspro/CmsPro.Infrastructure/Migrations/20260329025419_Feature_TestimonialSchema_Complete.Designer.cs
+++ cmspro/CmsPro.Infrastructure/Migrations/20260329025419_Feature_TestimonialSchema_Complete.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CmsPro.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CmsPro.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260329025419_Feature_TestimonialSchema_Complete")]
+    partial class Feature_TestimonialSchema_Complete
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
